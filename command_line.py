fc7958fce682c03f3ee7dfb69c412f84792a18f5
rayanezaz <rezaz@calpoly.edu>
2024-11-11 23:32:28 -0800

    Completed Lab 7

--- command_line.py
+++ command_line.py
@@ -1,1 +1,19 @@
+import sys
+from convert import str_to_float
 
+""'"'"""""""""
+This function takes the sys.argv that is called, and then it checks to see if each value in the list
+is a float. If the value is a float, then it is added to the total. However, if the value is not a float,
+then the function will still go through the other values in the list, and will return the total value for
+the float values added up.
+"""""""""
+
+
+if __name__ == '__main__':
+    print(sys.argv)
+    y = 0
+    for x in range(1, len(sys.argv)):
+        val = str_to_float(sys.argv[x])
+        if val is not None:
+            y += val
+    print(round(y, 3))
--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,29 @@
+import unittest
+import convert
 
+class TestCases(unittest.TestCase):
+
+    #Test 1
+    def testStrToFloat1(self):
+        input = "98.325"
+        expected = 98.325
+        result = convert.str_to_float(input)
+        self.assertEqual(expected, result)
+
+    #Test 2
+    def testStrToFloat2(self):
+        input = "room"
+        expected = None
+        result = convert.str_to_float(input)
+        self.assertEqual(expected, result)
+
+    #Test 3
+    def testStrToFloat3(self):
+        input = "25"
+        expected = None
+        result = convert.str_to_float(input)
+        self.assertEqual(expected, result)
+
+
+    if __name__ == '__main__':
+        unittest.main()
